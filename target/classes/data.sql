f954db37ce27d8e8309f4fb1a64da3169199401e
mehul400081 <mehul.bhatt2004@gmail.com>
2018-08-09 23:26:39 +0200

    added test

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO Employee (id, first_name, last_name) VALUES (1, 'Gary','Ray');
+INSERT INTO Employee (id, first_name, last_name) VALUES (2, 'Evil','MiniMie');
+INSERT INTO Employee (id, first_name, last_name) VALUES (3, 'Chuck','Norris');
+INSERT INTO Employee (id, first_name, last_name) VALUES (4, 'Austin','Powers');
